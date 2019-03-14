5de052774291c5c05ea23651cb0c96927b6c52f4
Deven Phillips <deven.phillips@gmail.com>
2019-03-14 07:37:51 -0700

    Fixed a bug in the script

--- roles/add-nexus-repo/files/add-hosted-yum-repository.groovy
+++ roles/add-nexus-repo/files/add-hosted-yum-repository.groovy
@@ -1,18 +1,13 @@
-import org.sonatype.nexus.repository.maven.LayoutPolicy;
-import org.sonatype.nexus.repository.maven.VersionPolicy;
 import org.sonatype.nexus.repository.storage.WritePolicy;
 import org.sonatype.nexus.blobstore.api.BlobStoreManager;
 
-def versionPolicy = VersionPolicy.valueOf('{{ version_policy | default("RELEASE") }}')
 def writePolicy = WritePolicy.valueOf('{{ write_policy | default("ALLOW") }}')
-def layoutPolicy = LayoutPolicy.valueOf('{{ layout_policy | default("STRICT") }}')
 def strictContentTypeValidation = '{{ strict_content_type_validation | default("false") }}'.matches(/(true,TRUE,t,y,Y,YES)/)
 
 if ( !repository.repositoryManager.exists( '{{ resource_name }}' ) ){
     repository.createMavenHosted('{{ resource_name }}', 
                                  BlobStoreManager.DEFAULT_BLOBSTORE_NAME,
                                  strictContentTypeValidation,
-                                 versionPolicy,
                                  writePolicy,
-                                 layoutPolicy)
+                                 {{ yum_repo_base_depth | default('1') }})
 };
