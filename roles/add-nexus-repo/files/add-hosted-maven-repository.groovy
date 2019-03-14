7f36369114cb464d306da84584f851b71bb25d80
Deven Phillips <deven.phillips@gmail.com>
2019-03-14 09:12:37 -0700

    More debugging of Yum and tests

--- roles/add-nexus-repo/files/add-hosted-maven-repository.groovy
+++ roles/add-nexus-repo/files/add-hosted-maven-repository.groovy
@@ -6,7 +6,13 @@ import org.sonatype.nexus.blobstore.api.BlobStoreManager;
 def writePolicy = WritePolicy.valueOf('{{ write_policy | default("allow") }}'.toUpperCase())
 def versionPolicy = VersionPolicy.valueOf('{{ version_policy | default("release") }}'.toUpperCase())
 def layoutPolicy = LayoutPolicy.valueOf('{{ layout_policy | default("strict") }}'.toUpperCase())
+def strictContentTypeValidation = '{{ strict_content_type_validation | default("false") }}'.matches(/(true,TRUE,t,y,Y,YES)/)
 
 if ( !repository.repositoryManager.exists( '{{ resource_name }}' ) ){
-    repository.createMavenHosted( '{{ resource_name }}', BlobStoreManager.DEFAULT_BLOBSTORE_NAME, true, versionPolicy, writePolicy, layoutPolicy)
+    repository.createMavenHosted('{{ resource_name }}', 
+                                 BlobStoreManager.DEFAULT_BLOBSTORE_NAME, 
+                                 strictContentTypeValidation, 
+                                 versionPolicy, 
+                                 writePolicy, 
+                                 layoutPolicy)
 };
--- roles/add-nexus-repo/files/add-hosted-yum-repository.groovy
+++ roles/add-nexus-repo/files/add-hosted-yum-repository.groovy
@@ -5,7 +5,7 @@ def writePolicy = WritePolicy.valueOf('{{ write_policy | default("ALLOW") }}')
 def strictContentTypeValidation = '{{ strict_content_type_validation | default("false") }}'.matches(/(true,TRUE,t,y,Y,YES)/)
 
 if ( !repository.repositoryManager.exists( '{{ resource_name }}' ) ){
-    repository.createMavenHosted('{{ resource_name }}', 
+    repository.createYumHosted('{{ resource_name }}', 
                                  BlobStoreManager.DEFAULT_BLOBSTORE_NAME,
                                  strictContentTypeValidation,
                                  writePolicy,
