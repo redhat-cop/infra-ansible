d8465dda3db4dfd295a4d4413b92fa6d2c3bdd6c
Deven Phillips <deven.phillips@gmail.com>
2019-03-16 07:14:21 -0400

    Added role for configuring Nexus remote user token

---
+++ roles/config-nexus-rut/files/configure-remote-user-token.groovy
@@ -0,0 +1,29 @@
+import groovy.json.JsonOutput
+import org.sonatype.nexus.capability.CapabilityReference
+import org.sonatype.nexus.capability.CapabilityType
+import org.sonatype.nexus.internal.capability.DefaultCapabilityReference
+import org.sonatype.nexus.internal.capability.DefaultCapabilityRegistry
+
+returnValue = JsonOutput.toJson([result : 'Did NOT add Rut Auth'])
+
+def capabilityRegistry = container.lookup(DefaultCapabilityRegistry.class.getName())
+
+//Capability specific values/properties
+def capabilityType = CapabilityType.capabilityType("rutauth")
+def capabilityProps = ['httpHeader': '{{ nexus_rut_header }}']
+def capabilityNotes = 'configured through scripting api'
+
+//check if existing Rut Auth capability exists
+DefaultCapabilityReference existing = capabilityRegistry.all.find { CapabilityReference capabilityReference ->
+  capabilityReference.context().descriptor().type() == capabilityType
+}
+
+//If it doesn't, add it with given values/properties
+//This should also enable the rutauth-realm
+if (!existing) {
+  log.info('Rut Auth capability created as: {}', capabilityRegistry.add(capabilityType, true, capabilityNotes, capabilityProps).toString())
+
+  returnValue = JsonOutput.toJson([result : 'Successfully added Rut Auth!'])
+}
+
+return returnValue
