62571a7445716b282885c511571ab195d5cece06
Benjamin Kincaid <bkincaid@redhat.com>
2019-03-27 13:28:37 -0400

    add ability to reset ipa user passwords

--- roles/identity-management/manage-idm-identities/filter_plugins/set-user-flags.py
+++ roles/identity-management/manage-idm-identities/filter_plugins/set-user-flags.py
@@ -1,14 +1,20 @@
-def set_user_flags(entry):
+def set_user_flags(entry, password_reset_users):
 
     data = {
         'generate_password': False,
         'notify_user': False
     }
 
-    if 'user' in entry.keys() and 'has_password' in entry['user'].keys() and entry['user']['has_password'] == False:
+    # flag if user's password has never been set
+    no_password = 'user' in entry.keys() and 'has_password' in entry['user'].keys() and entry['user']['has_password'] == False
+
+    # flag if password reset has been requested for user
+    passowrd_reset_requested = 'user' in entry.keys() and entry['user']['uid'][0] in password_reset_users
+
+    if no_password or passowrd_reset_requested:
         data['generate_password'] = True
         data['notify_user'] = True
-
+        
     return data
 
 
