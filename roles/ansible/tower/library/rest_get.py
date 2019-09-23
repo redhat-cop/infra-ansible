bb5a2dc287b044822d90c540113d7cf63529346e
Petter Abrahamsson <pabrahamsson@jebus.nu>
2019-09-23 15:49:59 -0400

    Add support for Ansible Tower settings, fixes #390

--- roles/ansible/tower/library/rest_get.py
+++ roles/ansible/tower/library/rest_get.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+import re
 import requests
-import json
 
 """
 An ansible module to wrap the calls to Ansible Tower REST API (and potentially others).
@@ -30,48 +30,55 @@ EXAMPLES = '''
 
 
 def main():
-  module = AnsibleModule(
-    argument_spec=dict(
-      host_url=dict(required=True),
-      api_uri=dict(required=True),
-      rest_user=dict(required=True),
-      rest_password=dict(required=True, no_log=True),
-    ),
-    supports_check_mode=True,
-  )
+    module = AnsibleModule(
+        argument_spec=dict(
+            host_url=dict(required=True),
+            api_uri=dict(required=True),
+            rest_user=dict(required=True),
+            rest_password=dict(required=True, no_log=True),
+        ),
+        supports_check_mode=True,
+    )
 
-  # Initialize the output list to empty
-  rest_output = []
+    # Retrieve the parameters passed in
+    host_url         = module.params['host_url']
+    api_uri          = module.params['api_uri']
+    rest_user        = module.params['rest_user']
+    rest_password    = module.params['rest_password']
 
-  # Retrieve the parameters passed in
-  host_url         = module.params['host_url']
-  api_uri          = module.params['api_uri']
-  rest_user        = module.params['rest_user']
-  rest_password    = module.params['rest_password']
+    # Initialize the output to empty list or dict
+    if re.match('/api/v[12]/settings', api_uri):
+        rest_output = {}
+    else:
+        rest_output = []
 
-  # Loop till the 'next' element contains Null/None
-  while api_uri != None:
-    r = requests.get(
-          '{0}{1}'.format(host_url, api_uri),
-          auth=(rest_user, rest_password),
-          headers={
-            'Content-Type': 'application/json',
-            'Accept': 'application/json'
-          },
-          verify=False
+    # Loop till the 'next' element contains Null/None
+    while api_uri is not None:
+        r = requests.get(
+            '{0}{1}'.format(host_url, api_uri),
+            auth=(rest_user, rest_password),
+            headers={
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            },
+            verify=False
         )
 
-    # Fetch the next URI returned by the REST call
-    # TODO: better error handling when REST service returns non-200 OK
-    api_uri = r.json()['next']
+        # Fetch the next URI returned by the REST call
+        # TODO: better error handling when REST service returns non-200 OK
+        api_uri = r.json().get('next', None)
 
-    # Merge the results json to the output list of items
-    rest_output = rest_output + r.json()['results']
+        # Merge the results json to the output list of items
+        if isinstance(rest_output, list):
+            rest_output = rest_output + r.json()['results']
+        else:
+            rest_output = r.json()
 
-  module.exit_json(
-    changed=False,
-    rest_output=rest_output
-  )
+    module.exit_json(
+      changed=False,
+      rest_output=rest_output
+    )
+
 
 if __name__ == '__main__':
     main()
