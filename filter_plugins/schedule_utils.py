e567f0e4e5a2c8ea2c271543a4feaf7fc9fd7a15
Paul Barfuss <pbarfuss@redhat.com>
2020-12-14 18:37:47 -0500

    adding some logic for different versions of rrule to process in correct format

--- filter_plugins/schedule_utils.py
+++ filter_plugins/schedule_utils.py
@@ -2,20 +2,24 @@ from datetime import timedelta
 from dateutil.parser import parse
 from dateutil.rrule import rrule
 
+
 def parse_datetime(datestring):
 
     # Parses ISO date string and converts to datetime object.
     return parse(datestring)
+
 
 def add_time(dt, **kwargs):
 
     # Calculate future datetime object.
     return dt + timedelta(**kwargs)
 
+
 def subtract_time(dt, **kwargs):
 
     # Calculate past datetime object.
     return dt - timedelta(**kwargs)
+
 
 def to_rrule(dt, **kwargs):
 
@@ -27,11 +31,15 @@ def to_rrule(dt, **kwargs):
     Note that interval is required by the Tower/AWX api but omitted
     by the rrule module if = 1
     """
-    r_rule = str(rrule(dtstart=dt, **kwargs)).split('\n')
-    parts = ['Z RRULE:' + r_rule[1]]
-    if 'INTERVAL' not in r_rule[1]:
-        parts.append('INTERVAL=1')
-    return r_rule[0] + ';'.join(parts)
+    r_rule = str(rrule(dtstart=dt, **kwargs)).split("\n")
+    if "RRULE" not in r_rule[1]:
+        parts = ["Z RRULE:" + r_rule[1]]
+    else:
+        parts = ["Z " + r_rule[1]]
+    if "INTERVAL" not in r_rule[1]:
+        parts.append("INTERVAL=1")
+    return r_rule[0] + ";".join(parts)
+
 
 class FilterModule(object):
 
@@ -40,10 +48,11 @@ class FilterModule(object):
     then add or subtract time before converting to an Ansible Tower/AWX compatible
     recurrence rule (rrule).
     """
+
     def filters(self):
         return {
-            'parse_datetime': parse_datetime,
-            'add_time': add_time,
-            'subtract_time': subtract_time,
-            'to_rrule': to_rrule,
+            "parse_datetime": parse_datetime,
+            "add_time": add_time,
+            "subtract_time": subtract_time,
+            "to_rrule": to_rrule,
         }
