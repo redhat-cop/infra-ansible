eb15fb3a2c19dfd20943f1b187fc7f7d182f9416
Paul Barfuss <pbarfuss@redhat.com>
2021-05-18 16:48:39 -0400

    Update schedule_utils filter plugin

--- filter_plugins/schedule_utils.py
+++ filter_plugins/schedule_utils.py
@@ -1,12 +1,31 @@
-from datetime import timedelta
+from ansible.errors import AnsibleError
+from datetime import datetime, timedelta
 from dateutil.parser import parse
-from dateutil.rrule import rrule
+from dateutil.rrule import rrule, DAILY
+import pytz
 
 
 def parse_datetime(datestring):
 
     # Parses ISO date string and converts to datetime object.
     return parse(datestring)
+
+
+def set_time(dt, hour, minute):
+
+    # Set the time to a specific hour, minute
+    return dt.replace(hour=hour, minute=minute)
+
+
+def set_timezone(dt, tz):
+
+    # Unset existing timezone
+    dt = dt.replace(tzinfo=None)
+    # Set the timezone
+    tz = pytz.timezone(tz)
+
+    # Normalize the timezone offset based on the datetime
+    return tz.normalize(tz.localize(dt))
 
 
 def add_time(dt, **kwargs):
@@ -24,21 +43,32 @@ def subtract_time(dt, **kwargs):
 def to_rrule(dt, **kwargs):
 
     """
-    Converts datetime object to recurrence rule in the format required by the
-    Ansible Tower/AWX /api/v2/project/{id}/schedules/ endpoint:
-    DTSTART:YYYYMMDDTHHMMSSZ RRULE:FREQ=MINUTELY,INTERVAL=10,COUNT=5
-
-    Note that interval is required by the Tower/AWX api but omitted
-    by the rrule module if = 1
+    Converts datetime object to recurrence rule in the format accepted by the
+    Ansible Tower /api/v2/{resource}/{id}/schedules/ endpoint
     """
-    r_rule = str(rrule(dtstart=dt, **kwargs)).split("\n")
-    if "RRULE" not in r_rule[1]:
-        parts = ["Z RRULE:" + r_rule[1]]
-    else:
-        parts = ["Z " + r_rule[1]]
-    if "INTERVAL" not in r_rule[1]:
-        parts.append("INTERVAL=1")
-    return r_rule[0] + ";".join(parts)
+
+    options = {
+        "freq": DAILY,
+        "interval": 1,
+        "count": 1,
+        "timezone": "Zulu",
+    }
+    options.update(kwargs)
+    # Pop timezone from options for later use
+    timezone = options.pop("timezone")
+    my_rrule = rrule(dtstart=dt, **options)
+    # Validate timezone name and add to RRULE using some string manipulation
+    if timezone not in pytz.all_timezones:
+        raise AnsibleError("Timezone is not valid")
+    return_rrulestr = (
+        str(my_rrule)
+        .replace("\n", " ")
+        .replace("DTSTART:", "DTSTART;TZID={0}:".format(timezone))
+    )
+    # Add interval again as required by Tower API if omitted by rrule module
+    if "INTERVAL" not in return_rrulestr:
+        return_rrulestr = "{0};INTERVAL=1".format(return_rrulestr)
+    return return_rrulestr
 
 
 class FilterModule(object):
@@ -52,6 +82,8 @@ class FilterModule(object):
     def filters(self):
         return {
             "parse_datetime": parse_datetime,
+            "set_time": set_time,
+            "set_timezone": set_timezone,
             "add_time": add_time,
             "subtract_time": subtract_time,
             "to_rrule": to_rrule,
